040ad653c46a0a29abc597bde4805dd53102d00b
Vijay Ramamurthy <vrama628@gmail.com>
2025-06-27 21:53:39 -0400

    3.9

--- src/sqlalchemy_oso_cloud/query.py
+++ src/sqlalchemy_oso_cloud/query.py
@@ -1,13 +1,14 @@
 import sqlalchemy.orm
 from sqlalchemy import text
 from oso_cloud import Value
-from typing import TypeVar, Self
+from typing import TypeVar
 from .oso import get_oso
 
 T = TypeVar("T")
+Self = TypeVar("Self", bound="Query")
 
 class Query(sqlalchemy.orm.Query[T]):
-  def authorized_for(self, actor: Value, action: str) -> Self:
+  def authorized_for(self: Self, actor: Value, action: str) -> Self:
     for col in self._raw_columns:
       print(col)
     authorization_filter = get_oso().list_local(actor, action, "Document", "document.id") # TODO
