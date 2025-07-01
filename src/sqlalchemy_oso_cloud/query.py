7f7971939112aeae277b74dbd0d1592c6575ccbe
conniedonahue <143646863+conniedonahue@users.noreply.github.com>
2025-07-01 16:56:49 -0400

    Connor/eng 1898 build authorization filtering via query subclass (#8)

--- src/sqlalchemy_oso_cloud/query.py
+++ src/sqlalchemy_oso_cloud/query.py
@@ -1,5 +1,6 @@
 import sqlalchemy.orm
-from sqlalchemy import text
+from sqlalchemy.orm import with_loader_criteria
+from sqlalchemy import literal_column, ColumnClause
 from oso_cloud import Value
 from typing import TypeVar
 from .oso import get_oso
@@ -7,12 +8,18 @@ from .oso import get_oso
 T = TypeVar("T")
 Self = TypeVar("Self", bound="Query")
 
+#TODO - multiple permissions for multiple main models
 class Query(sqlalchemy.orm.Query[T]):
   """
   An extension of [`sqlalchemy.orm.Query`](https://docs.sqlalchemy.org/orm/queryguide/query.html#sqlalchemy%2Eorm%2EQuery)
   that adds support for authorization.
   """
-  def authorized_for(self: Self, actor: Value, action: str) -> Self:
+  
+  def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.oso = get_oso()
+
+  def authorized(self: Self, actor: Value, action: str) -> Self:
     """
     Filter the query to only include resources that the given actor is authorized to perform the given action on.
 
@@ -21,7 +28,41 @@ class Query(sqlalchemy.orm.Query[T]):
 
     :return: A new query that includes only the resources that the actor is authorized to perform the action on.
     """
-    for col in self._raw_columns:
-      print(col)
-    authorization_filter = get_oso().list_local(actor, action, "Document", "document.id") # TODO
-    return self.where(text(authorization_filter))
+    models = self._extract_unique_models()
+    options = []
+
+    for model in models:
+        auth_criteria = self._create_auth_criteria_for_model(model, actor, action)
+        options.append(
+            with_loader_criteria(
+                model, 
+                auth_criteria,
+                include_aliases=True
+            )
+        )
+
+    return self.options(*options)
+  
+  def _extract_unique_models(self):
+    """Extract all models being queried"""
+
+    models = set()
+    
+    for desc in self.column_descriptions:
+        if desc['entity'] is not None:
+            models.add(desc['entity'])
+
+    return models
+
+  def _create_auth_criteria_for_model(self, model, actor: Value, action: str):
+        """Create auth criteria"""
+
+        sql_filter = self.oso.list_local(
+            actor=actor,
+            action=action,
+            resource_type=model.__name__,
+            column=f"{model.__tablename__}.id"
+        )
+        criteria: ColumnClause = literal_column(sql_filter)   
+
+        return lambda cls: criteria
--- tests/test_main.py
+++ tests/test_main.py
@@ -21,12 +21,12 @@ def test_manual(oso: Oso, session: Session, alice: Value, bob: Value):
   assert any(document.id == 3 for document in documents) # bob can see this because it is "public"
 
 def test_library(oso_session: sqlalchemy_oso_cloud.Session, alice: Value, bob: Value):
-  documents = oso_session.query(Document).authorized_for(alice, "read").all()
+  documents = oso_session.query(Document).authorized(alice, "read").all()
   assert len(documents) == 3
   assert any(document.id == 1 for document in documents)
   assert any(document.id == 2 for document in documents) # alice can see this because it is "published"
   assert any(document.id == 3 for document in documents) # alice can see this because it is "public"
-  documents = oso_session.query(Document).authorized_for(bob, "read").all()
+  documents = oso_session.query(Document).authorized(bob, "read").all()
   assert len(documents) == 2
   assert any(document.id == 2 for document in documents)
   assert any(document.id == 3 for document in documents) # bob can see this because it is "public"
@@ -34,3 +34,32 @@ def test_library(oso_session: sqlalchemy_oso_cloud.Session, alice: Value, bob: V
 def test_local_authorization_config_snapshot(snapshot):
   config = sqlalchemy_oso_cloud.oso.generate_local_authorization_config(Base.registry)
   snapshot.assert_match(yaml.dump(config))
+
+def test_bob_can_read_different_document(oso_session: sqlalchemy_oso_cloud.Session, bob: Value):
+  documents = oso_session.query(Document).authorized(bob, "read").all()
+  assert len(documents) == 2
+  assert documents[0].id == 2
+
+def test_bob_cannot_eat_documents(oso_session: sqlalchemy_oso_cloud.Session, bob: Value):
+  documents = oso_session.query(Document).authorized(bob, "eat").all()
+  assert len(documents) == 0
+
+def test_authorize_with_filter(oso_session: sqlalchemy_oso_cloud.Session, alice: Value):
+  documents = (
+      oso_session.query(Document)
+      .filter(Document.id == 1)
+      .authorized(alice, "read")
+      .filter(Document.content == "hello")
+      .all()
+  )
+  assert len(documents) == 1
+  assert documents[0].id == 1
+
+def test_authorize_doesnt_bring_in_filtered(oso_session: sqlalchemy_oso_cloud.Session, bob: Value):
+  documents = (
+      oso_session.query(Document)
+      .filter(Document.id == 1)
+      .authorized(bob, "read")
+      .all()
+  )
+  assert len(documents) == 0
