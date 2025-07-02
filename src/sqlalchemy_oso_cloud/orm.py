f6b6895691d9e27c34367f605d5d96c72e7fa044
Iris Hu <ihu009@gmail.com>
2025-07-02 16:35:25 -0400

    static type checking for remote_relation :D

--- src/sqlalchemy_oso_cloud/orm.py
+++ src/sqlalchemy_oso_cloud/orm.py
@@ -4,9 +4,9 @@ Utilities for [declaratively mapping](https://docs.sqlalchemy.org/en/20/orm/mapp
 """
 import inspect
 
-from typing import Callable, Any, Optional, Dict
-from typing_extensions import ParamSpec, TypeVar
-from functools import wraps
+from typing import Callable, Optional, Protocol, Any
+from typing_extensions import ParamSpec, TypeVar, Concatenate
+from functools import partial, wraps
 
 from sqlalchemy.orm import MappedColumn, Relationship, mapped_column, relationship
 from typing import Union
@@ -23,67 +23,31 @@ _REMOTE_RELATION_INFO_KEY = "_oso.remote_relation"
 
 P = ParamSpec('P')
 T = TypeVar('T')
+R = TypeVar('R', covariant=True)
 
-def wrap(func: Callable[P, Any]) -> Callable[[Callable[P, T]], Callable[P, T]]:
+def _wrap(func: Callable[P, Any]) -> Callable[[Callable[P, T]], Callable[P, T]]:
     """Wrap a SQLAlchemy function in a type-safe way."""
     def decorator(wrapper: Callable[P, T]) -> Callable[P, T]:
-        @wraps(func)
-        def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
-            return wrapper(*args, **kwargs)
-        return wrapped
+      @wraps(func)
+      def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
+          return wrapper(*args, **kwargs)
+      return wrapped
     return decorator
 
-F = TypeVar('F', bound=Callable[..., Any])
+class _WithExtraKwargs(Protocol[P, R]):
+    def __call__(self, remote_resource_name: str, remote_relation_key: Optional[str] = None, *args: P.args, **kwargs: P.kwargs) -> R:
+        ...
 
-def wrap_with_signature(
-    wrapped_func: Callable,
-    extra_params: Optional[Dict[str, Dict[str, Any]]] = None
-):
-    """
-    Decorator that copies signature from wrapped_func and optionally adds extra parameters.
-    
-    Args:
-        wrapped_func: The function whose signature to copy
-        extra_params: Dict of {param_name: {'annotation': type, 'default': value}}
-    """
-    def decorator(wrapper_func: F) -> F:
-        # Get the original signature
-        sig = inspect.signature(wrapped_func)
-        params = list(sig.parameters.values())
-        
-        # Add extra parameters as keyword-only
-        if extra_params:
-            for name, info in extra_params.items():
-                param = inspect.Parameter(
-                    name,
-                    inspect.Parameter.KEYWORD_ONLY,
-                    default=info.get('default', inspect.Parameter.empty),
-                    annotation=info.get('annotation', inspect.Parameter.empty)
-                )
-                params.append(param)
-        
-        # Create new signature
-        new_sig = sig.replace(parameters=params)
-        
-        # Apply to wrapper function
-        wrapper_func.__signature__ = new_sig
-        wrapper_func.__annotations__ = getattr(wrapped_func, '__annotations__', {})
-        
-        # Add extra param annotations
-        if extra_params:
-            for name, info in extra_params.items():
-                if 'annotation' in info:
-                    wrapper_func.__annotations__[name] = info['annotation']
-        
-        wrapper_func.__name__ = wrapped_func.__name__
-        wrapper_func.__doc__ = wrapped_func.__doc__
-        wrapper_func.__module__ = wrapped_func.__module__
-        
-        return wrapper_func
-    
-    return decorator
+def _add_params(wrapped_func: Callable[P, R]) -> Callable[[_WithExtraKwargs[P, R]], _WithExtraKwargs[P, R]]:
+  """Adds extra keyword parameters to `remote_relation` in order to support static type checking."""
+  def decorator(wrapper: Callable[Concatenate[str, Optional[str], P], R]) -> _WithExtraKwargs[P, R]:
+    @wraps(wrapped_func)
+    def wrapped(remote_resource_name: str, remote_relation_key: Union[str, None] = None, *args: P.args, **kwargs: P.kwargs) -> R:
+      return wrapper(remote_resource_name, remote_relation_key, *args, **kwargs)
+    return wrapped
+  return decorator
 
-@wrap_with_signature(relationship)
+@_wrap(relationship)
 def relation(*args, **kwargs) -> Relationship:
   """
   A wrapper around [`sqlalchemy.orm.relationship`](https://docs.sqlalchemy.org/en/20/orm/relationship_api.html#sqlalchemy.orm.relationship)
@@ -98,7 +62,7 @@ def relation(*args, **kwargs) -> Relationship:
   rel.info[_RELATION_INFO_KEY] = None
   return rel
 
-@wrap(mapped_column)
+@_wrap(mapped_column)
 def attribute(*args, **kwargs) -> MappedColumn:
   """
   A wrapper around [`sqlalchemy.orm.mapped_column`](https://docs.sqlalchemy.org/en/20/orm/mapping_api.html#sqlalchemy.orm.mapped_column)
@@ -112,7 +76,8 @@ def attribute(*args, **kwargs) -> MappedColumn:
   col.column.info[_ATTRIBUTE_INFO_KEY] = None
   return col
 
-def remote_relation(remote_resource_name: str, remote_relation_key: Union[str, None] = None, *args, **kwargs) -> MappedColumn:
+@_add_params(mapped_column)
+def remote_relation(remote_resource_name: str, remote_relation_key: Optional[str] = None, *args, **kwargs) -> MappedColumn:
   """
   A wrapper around [`sqlalchemy.orm.mapped_column`](https://docs.sqlalchemy.org/en/20/orm/mapping_api.html#sqlalchemy.orm.mapped_column)
   that indicates that the attribute corresponds to `has_relation` facts (to a resource not defined in the local database) in Oso with the following two arguments:
