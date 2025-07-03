b089c253394cf601fd722a65ca9be29d88059434
conniedonahue <connor@osohq.com>
2025-07-03 19:28:39 -0400

    Add overloads to session.py for query

--- src/sqlalchemy_oso_cloud/session.py
+++ src/sqlalchemy_oso_cloud/session.py
@@ -1,8 +1,14 @@
 import sqlalchemy.orm
 from .query import Query
-from typing import Type, TypeVar
+from sqlalchemy.engine import Row
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+from typing import Type, TypeVar, overload, Any, Tuple
 
 T = TypeVar("T")
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
+T4 = TypeVar("T4")
 
 class Session(sqlalchemy.orm.Session):
   """
@@ -21,16 +27,49 @@ class Session(sqlalchemy.orm.Session):
     Initialize a SQLAlchemy session with the `.Query` class extended to support authorization.
     Accepts all of the same arguments as [`sqlalchemy.orm.Session`](https://docs.sqlalchemy.org/orm/session_api.html#sqlalchemy%2Eorm%2ESession),
     except for `query_cls`.
-    """
+      """
     if "query_cls" in kwargs:
       raise ValueError("sqlalchemy_oso_cloud does not currently support combining with other query classes")
     super().__init__(*args, **{ **kwargs, "query_cls": Query })
 
-  # TODO(connor): arguments/types to support original query method
-  def query(self, entity: Type[T], *args, **kwargs) -> Query[T]:  # type: ignore
-    """
-    Returns a SQLAlchemy query extended to support authorization.
-    Accepts all of the same arguments as
-    [`sqlalchemy.orm.Session.query`](https://docs.sqlalchemy.org/en/20/orm/session_api.html#sqlalchemy%2Eorm%2ESession%2Equery).
-    """
-    return super().query(entity, *args, **kwargs)
+  # Single entity overload
+  @overload # type: ignore[override]
+  def query(self, entity: Type[T], /) -> Query[T]: ...
+
+  # Single column overload
+  @overload
+  def query(self, column: InstrumentedAttribute[T], /) -> Query[Row[Tuple[T]]]: ...
+
+  # Two entities overload
+  @overload
+  def query(self, entity1: Type[T1], entity2: Type[T2], /) -> Query[Row[Tuple[T1, T2]]]: ...
+
+  # Two columns overload
+  @overload
+  def query(self, col1: InstrumentedAttribute[T1], col2: InstrumentedAttribute[T2], /) -> Query[Row[Tuple[T1, T2]]]: ...
+
+  # Mixed: entity + column
+  @overload
+  def query(self, entity: Type[T1], column: InstrumentedAttribute[T2], /) -> Query[Row[Tuple[T1, T2]]]: ...
+
+  # Three entities overload
+  @overload
+  def query(self, entity1: Type[T1], entity2: Type[T2], entity3: Type[T3], /) -> Query[Row[Tuple[T1, T2, T3]]]: ...
+
+  # Four entities overload
+  @overload
+  def query(self, entity1: Type[T1], entity2: Type[T2], entity3: Type[T3], entity4: Type[T4], /) -> Query[Row[Tuple[T1, T2, T3, T4]]]: ...
+
+  # Fallback overload
+  @overload
+  def query(self, *entities: Any) -> Query[Any]: ...
+
+  def query(self, *entities, **kwargs) -> Query[Any]:
+      """
+      Returns a SQLAlchemy query extended to support authorization.
+
+      Single entity queries which return Query[T].
+      Multi-entity and column queries return Query[Row[Tuple[...]]].
+      All other queries types return Query[Any].
+      """
+      return super().query(*entities, **kwargs)
