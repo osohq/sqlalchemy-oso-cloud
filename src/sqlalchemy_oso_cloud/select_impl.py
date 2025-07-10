d1fa03ad927f97e313fd824fe7af5ad0834a69c5
conniedonahue <connor@osohq.com>
2025-07-09 20:16:38 -0400

    Add overloads to select and execute

--- src/sqlalchemy_oso_cloud/select_impl.py
+++ src/sqlalchemy_oso_cloud/select_impl.py
@@ -1,11 +1,18 @@
 import sqlalchemy.sql
+from sqlalchemy.orm.attributes import InstrumentedAttribute
 from oso_cloud import Value
-from typing import  TypeVar
+from typing import  TypeVar, Generic, Any, overload, Type, Tuple, Union
 from .auth import _apply_authorization_options
 
 Self = TypeVar("Self", bound="Select")
+_T = TypeVar("_T", bound=Tuple[Any, ...])
+T = TypeVar("T")
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
+T4 = TypeVar("T4")
 
-class Select(sqlalchemy.sql.Select):
+class Select(sqlalchemy.sql.Select[_T], Generic[_T]):
     """A Select subclass that adds authorization functionality"""
 
     inherit_cache = True
@@ -18,9 +25,34 @@ class Select(sqlalchemy.sql.Select):
     def authorized(self: Self, actor: Value, action: str) -> Self:
         """Add authorization filtering to the select statement"""
         return _apply_authorization_options(self, actor, action)
-    
-    
-def select(*args, **kwargs) -> Select:
+
+# Single entity overload
+@overload
+def select(entity: Type[T]) -> Select[Tuple[T]]: ...
+
+# Single column overload
+@overload
+def select(column: InstrumentedAttribute[T]) -> Select[Tuple[T]]: ...
+
+@overload
+def select(
+    arg1: Union[Type[T1], InstrumentedAttribute[T1]], 
+    arg2: Union[Type[T2], InstrumentedAttribute[T2]]) -> Select[Tuple[T1, T2]]: ...
+
+@overload
+def select(arg1: Union[Type[T1], InstrumentedAttribute[T1]], 
+           arg2: Union[Type[T2], InstrumentedAttribute[T2]], 
+           arg3: Union[Type[T3], InstrumentedAttribute[T3]]) -> Select[Tuple[T1, T2, T3]]: ...
+
+@overload
+def select(arg1: Union[Type[T1], InstrumentedAttribute[T1]], 
+           arg2: Union[Type[T2], InstrumentedAttribute[T2]], 
+           arg3: Union[Type[T3], InstrumentedAttribute[T3]], 
+           arg4: Union[Type[T4], InstrumentedAttribute[T4]]) -> Select[Tuple[T1, T2, T3, T4]]: ...
+
+@overload
+def select(*entities: Any) -> Select[Any]: ...    
+def select(*entities, **kwargs) -> Select[Any]:
     """
     Create an sqlalchemy_oso_cloud.Select() object
 
@@ -33,4 +65,4 @@ def select(*args, **kwargs) -> Select:
         stmt = select(Document).where(Document.private == True).authorized(actor, "read")
         documents = session.execute(stmt)
     """
-    return Select(*args, **kwargs)
+    return Select(*entities, **kwargs)
